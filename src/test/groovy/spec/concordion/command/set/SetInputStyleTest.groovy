37dfd2fc203ce77edfc7ea95ea42af58cadce984
Tommy Yim Li <tommy.li@firefire.co>
2014-12-06 15:52:19 +1100

    Examples of using concordion:set with styling

---
+++ src/test/groovy/spec/concordion/command/set/SetInputStyleTest.groovy
@@ -0,0 +1,15 @@
+// Copyright Red Energy Limited 2014
+// Tommy Li (tommy.li@redenergy.com.au), 2014-12-06
+
+package spec.concordion.command.set
+
+import org.concordion.integration.junit4.ConcordionRunner
+import org.junit.runner.RunWith
+
+@RunWith(ConcordionRunner.class)
+class SetInputStyleTest {
+
+  Integer add(Integer a, Integer b) {
+    a + b
+  }
+}
